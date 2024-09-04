f5d7edf2245440696b2b2c7a481ee8d49c1da266
Tho Le <thodoanle26@gmail.com>
2024-09-04 13:51:48 +0000

    feat: fix bug in tc 8 and 13

--- number_guess.sh
+++ number_guess.sh
@@ -1,65 +1,82 @@
 #!/bin/bash
-PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess --no-align --tuples-only -c"
+GUESS_COUNT=0
 
+# generate random number
+RANDOM_NUMBER=$[ $RANDOM % 1000 + 1 ]
+
+# get user's username
 echo "Enter your username:"
-read USER_NAME
+read USER
 
-LAST_QUERY=$($PSQL "SELECT name, games_played, takes FROM ranked WHERE name='$USER_NAME'")
+# check if username is in the database
+USERNAME_EXISTS=$($PSQL "SELECT name FROM ranked WHERE name='$USER'")
 
-# if name doesn't exist
-if [[ -z $LAST_QUERY ]]
+# if not found
+if [[ -z $USERNAME_EXISTS ]]
 then
-  echo "Welcome, $USER_NAME! It looks like this is your first time here."
-  GAMES=0
-  BEST=9999
+  # insert username in database
+  INSERT_USER_INFO_RESULT=$($PSQL "INSERT INTO ranked(name, games_played) VALUES('$USER', 0)")
+
+  # get user info
+  GAMES_PLAYED=$($PSQL "SELECT games_played FROM ranked WHERE name='$USER'")
+  BEST_GAME=$($PSQL "SELECT best FROM ranked WHERE name='$USER'")
+  USERNAME=$($PSQL "SELECT name FROM ranked WHERE name='$USER'")
+
+  # greet user
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+
+# if username is found
 else
-  IFS="|" read NAME GAMES BEST <<< "$LAST_QUERY"
-  echo "Welcome back, $NAME! You have played $GAMES games, and your best game took $BEST guesses."
+
+  # get user info
+  GAMES_PLAYED=$($PSQL "SELECT games_played FROM ranked WHERE name='$USER'")
+  BEST_GAME=$($PSQL "SELECT best FROM ranked WHERE name='$USER'")
+  USERNAME=$($PSQL "SELECT name FROM ranked WHERE name='$USER'")
+
+  # greet user
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
-# Create random number in range
-NUMBER_TO_GUESS=$(( RANDOM % 1000 + 1 ))
-CURRENT_TAKE=0
-
-QUERY_USER () {
-  if [[ -z $1 ]]
-  then
-    echo "Guess the secret number between 1 and 1000:"
-  else
-    echo "$1"
-  fi
-
+GUESS () {
   read USER_GUESS
-  CURRENT_TAKE=$(( CURRENT_TAKE + 1 ))
 
+  # if the guess is not an integer
   if [[ ! $USER_GUESS =~ ^[0-9]+$ ]]
   then
-    QUERY_USER "That is not an integer, guess again:"
-  else
-    if [[ $USER_GUESS -lt $NUMBER_TO_GUESS ]]
-    then
-      QUERY_USER "It's higher than that, guess again:"
-    elif [[ $USER_GUESS -gt $NUMBER_TO_GUESS ]]
-    then
-      QUERY_USER "It's lower than that, guess again:"
-    else
-      echo "You guessed it in $CURRENT_TAKE tries. The secret number was $NUMBER_TO_GUESS. Nice job!"
-      
-      NEW_GAMES=$(( GAMES + 1 ))
-
-      if [[ $CURRENT_TAKE -lt $BEST ]]
-      then
-        $PSQL "UPDATE ranked SET games_played=$NEW_GAMES, takes=$CURRENT_TAKE WHERE name='$USER_NAME'"
-      else
-        $PSQL "UPDATE ranked SET games_played=$NEW_GAMES WHERE name='$USER_NAME'"
-      fi
-
-      if [[ $GAMES -eq 0 ]]
-      then
-        $PSQL "INSERT INTO ranked(name, games_played, takes) VALUES('$USER_NAME', $NEW_GAMES, $CURRENT_TAKE)"
-      fi
-    fi
+    echo "That is not an integer, guess again:"
+    GUESS
+  # if the guess is higher than the secret number
+  elif [[ $USER_GUESS -gt $RANDOM_NUMBER ]]
+  then
+    let "GUESS_COUNT++"
+    echo "It's lower than that, guess again:"
+    GUESS
+  # if the guess is lower
+  elif [[ $USER_GUESS -lt $RANDOM_NUMBER ]]
+  then
+    let "GUESS_COUNT++"
+    echo "It's higher than that, guess again:"
+    GUESS
+  # if the guess is correct
+  elif [[ $USER_GUESS -eq $RANDOM_NUMBER ]]
+  then
+    let "GUESS_COUNT++"
+    echo "You guessed it in $GUESS_COUNT tries. The secret number was $RANDOM_NUMBER. Nice job!"
   fi
 }
 
-QUERY_USER
+echo "Guess the secret number between 1 and 1000:"
+GUESS
+
+# UPDATE USER INFO
+
+# update games_played
+UPDATE_GAMES_PLAYED=$($PSQL "UPDATE ranked SET games_played=ranked.games_played+1 WHERE name='$USERNAME'")
+
+# update best_game
+
+if [[ -z $BEST_GAME || $BEST_GAME -gt $GUESS_COUNT ]]
+then
+  UPDATE_BEST_GAME=$($PSQL "UPDATE ranked SET best=$GUESS_COUNT WHERE name='$USERNAME'")
+fi
