3efb7d775133320a6b4cbe1f6aefe5d25384af16
Tho Le <thodoanle26@gmail.com>
2024-09-04 11:59:30 +0000

    feat: add open query

--- number_guess.sh
+++ number_guess.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
+echo "Enter your username:"
+read USER_NAME
+
+LAST_QUERY=$($PSQL "SELECT name, MAX(games_played), MIN(takes) FROM ranked WHERE name='$USER_NAME' GROUP BY name")
+
+# if name doesn't exist
+if [[ -z $LAST_QUERY ]]
+then
+  echo "Welcome, $USER_NAME! It looks like this is your first time here."
+else
+  IFS="|" read NAME GAMES BEST <<< "$LAST_QUERY"
+  echo "Welcome back, $NAME! You have played $GAMES games, and your best game took $BEST guesses."
+fi
+
